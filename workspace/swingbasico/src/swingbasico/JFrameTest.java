3b4978010a6690f315b2ad884cdfbb4f6bd16e25
pepe navarro <oscar.novillo@gmail.com>
2016-02-19 21:37:04 +0100

    asdasd

--- workspace/swingbasico/src/swingbasico/JFrameTest.java
+++ workspace/swingbasico/src/swingbasico/JFrameTest.java
@@ -9,6 +9,10 @@ import javax.swing.border.EmptyBorder;
 
 public class JFrameTest extends JFrame {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
@@ -32,7 +36,7 @@ public class JFrameTest extends JFrame {
 	 */
 	public JFrameTest() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
