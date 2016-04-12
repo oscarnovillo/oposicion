eb8e92234ada2656acd85d553874f8bfb8e6f571
pepe navarro <oscar.novillo@gmail.com>
2016-04-12 08:35:38 +0000

    fff

---
+++ VBNET/ConsoleApplication1/Menu.vb
@@ -0,0 +1,48 @@
+﻿Public Class Menu
+
+    Public Property BackColor As ConsoleColor = ConsoleColor.Black
+    Public Property CenterTitle As Boolean = True
+    Public Property ForeColor As ConsoleColor = ConsoleColor.Green
+    Public Property Options As List(Of MenuOption) = New List(Of MenuOption)
+    Public Property Spacing As Integer = 3
+    Public Property Title As String = "Menu"
+
+    Public Function Show() As String
+        'Clear the console
+        Console.Clear()
+
+        'Set the color
+        Console.BackgroundColor = Me.BackColor
+        Console.ForegroundColor = Me.ForeColor
+
+        'Print the title
+        Console.CursorTop = 0 : Console.CursorLeft = If(Me.CenterTitle, (Console.BufferWidth \ 2) - (Me.Title.Length \ 2), 0)
+        Console.Write(Me.Title)
+
+        'Print the options
+        Console.CursorLeft = 0 : Console.CursorTop = 2
+        Dim left As Integer = Me.Options.Max(Function(m) m.Name.Length) + Me.Spacing
+        For i As Integer = 0 To Me.Options.Count - 1
+            Dim item As MenuOption = Me.Options.Item(i)
+            Console.Write((i + 1).ToString & ") " & item.Name)
+            Console.CursorLeft = left + (i + 1).ToString.Length + 1
+            Console.WriteLine(item.Description)
+        Next
+
+        'Finish
+        Return Console.ReadLine()
+    End Function
+
+End Class
+
+Public Class MenuOption
+
+    Public Sub New(description As String, name As String)
+        Me.Description = description
+        Me.Name = name
+    End Sub
+
+    Public Property Description As String
+    Public Property Name As String
+
+End Class
--- VBNET/ConsoleApplication1/Module1.vb
+++ VBNET/ConsoleApplication1/Module1.vb
@@ -1,10 +1,62 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("HOLA")
 
+        Dim menu As Menu
+        menu = New Menu()
+        menu.Options.Add(New MenuOption("programa1", "1"))
+        Dim op As String = menu.Show()
+        Select Case op
+            Case "1"
+                ejemplo1()
+            Case Else
+                Console.WriteLine("ADIOS")
+        End Select
+
+
+    End Sub
+
+
+    Sub ejemplo1()
+        Dim vector() As Integer
+        Dim longitud As Integer
+        Dim i As Integer = 0, j As Integer
+        Dim salir As Boolean
+        Try
+            Console.WriteLine("Introduzca el número de elementos")
+            longitud = Console.ReadLine()
+        Catch e As Exception
+            Console.WriteLine("El error ocurrido es : {0}", e.ToString)
+
+        End Try
+        longitud -= 1
+
+        ReDim vector(longitud)
+        For i = 0 To longitud
+            Do
+                Try
+                    Console.WriteLine("Introduzca el elemento {0}", i + 1)
+                    vector(i) = CType(Console.ReadLine(), Integer)
+                    salir = True
+                Catch e As InvalidCastException
+                    salir = False
+                    Console.WriteLine("El valor introducido no es un entero")
+                Catch e As Exception
+                    salir = False
+                    Console.WriteLine(e.ToString)
+                End Try
+            Loop Until salir
+        Next
+        j = 1
+        For i = 0 To vector.Length - 1
+            Console.Write("{0}{1}", vector(i).ToString, vbTab)
+            If j = 3 Then
+                Console.WriteLine()
+                j = 0
+            End If
+            j += 1
+        Next
         Console.ReadLine()
-
     End Sub
 
 End Module
